038ac47baf371cba9ba6f05af233b60ab7355a23
Ris <64377188+RisDN@users.noreply.github.com>
2021-11-26 10:31:12 +0100

    Create masodfoku_egyenlet.py

---
+++ Prog/Python/Egyéb/masodfoku_egyenlet.py
@@ -0,0 +1,22 @@
+import math
+a = int(input("Add meg az A-t: "))
+b = int(input("Add meg az B-t: "))
+c = int(input("Add meg az C-t: "))
+
+minusz_b = -1*b
+#print("MinuszB:", minusz_b)
+
+b_a_masodikon = math.pow(b, 2)
+#print("BaMasodikon:", b_a_masodikon)
+
+gyok_alatt = b_a_masodikon-(4*a*c)
+#print("Gyök alatt vonás előtt: ", gyok_alatt)
+
+gyokvonas_utan = math.sqrt(gyok_alatt)
+#print("Gyökvonás utan:", gyokvonas_utan)
+
+x1 = (minusz_b+gyokvonas_utan)/(2*a)
+#print("X1:", x1)
+
+x2 = (minusz_b-gyokvonas_utan)/(2*a)
+#print("X2:", x2)
