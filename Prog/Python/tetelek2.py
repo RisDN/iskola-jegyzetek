ee7e32738e29d979dc42a7f323391ea7941ad02d
Ris <64377188+RisDN@users.noreply.github.com>
2022-01-28 11:52:42 +0100

    Update tetelek2.py

--- Prog/Python/tetelek2.py
+++ Prog/Python/tetelek2.py
@@ -61,6 +61,29 @@ if M[maxM] > R[maxR]: print(f"A mókus csapat gyűjtött többet a {maxM+1}. hó
 else: print(f"A Róka csapat gyűjtött többet a {maxR+1}. hónapban")
 
 
+# Írassuk ki a hónapokat a havi összesen gyűjtött mennyiség szerint növekvő sorrendben!
+osszesitve = []
+for m, r in zip(M, R):
+    i += 1
+    osszesitve.append(m+r)
+
+
+indexek = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+for i in range(len(osszesitve)-1, 0, -1):
+    for j in range(0, i):
+        if(osszesitve[j] > osszesitve[j+1]):
+            tmp = osszesitve[j+1]
+            osszesitve[j+1] = osszesitve[j]
+            osszesitve[j] = tmp
+
+            tmp = indexek[j+1]
+            indexek[j+1] = indexek[j]
+            indexek[j] = tmp
+
+print(osszesitve)
+
+for i in range(1, len(indexek)):
+    print(f"{honap_szamito(indexek[i])}: {osszesitve[i]}")
 
 
 
