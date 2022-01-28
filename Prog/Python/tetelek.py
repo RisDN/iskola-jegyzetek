f01010d240888324ce5977a5295d6c6a663695e4
Ris <64377188+RisDN@users.noreply.github.com>
2022-01-28 10:22:41 +0100

    Create tetelek.py

---
+++ Prog/Python/tetelek.py
@@ -0,0 +1,41 @@
+from random import randint
+
+jegyek = []
+lehetosegek = [0, 0, 0, 0, 0]
+osszeg = 0
+
+for i in range(150):
+    jegy = randint(1, 5)
+    osszeg += jegy
+    lehetosegek[jegy-1] += 1
+    jegyek.append(jegy)
+
+print(lehetosegek, '\n')
+# Melyikből van a legtöbb?
+max = 0
+for i in range(1, len(lehetosegek)):
+    if lehetosegek[i] > lehetosegek[max]:
+        max = i
+print(f'A(z) {max+1} jegyből van a legtöbb: ')
+
+# Melyikből van a legkevesebb?
+min = 0
+for i in range(1, len(lehetosegek)):
+    if lehetosegek[i] < lehetosegek[min]:
+        min = i
+print(f'A(z) {min+1} jegyből van a legtöbb: ')
+
+# Van-e olyan osztályzat, amelynek a darabszáma pontosan az összes jegy 1/5 része?
+i = 0
+while i < len(lehetosegek) and lehetosegek[i] != len(jegyek)/5:
+    i += 1
+
+if i < len(lehetosegek): print("Van")
+else: print("Nincs")
+
+# Minden osztályzat előfordul legalább 5-ször?
+
+
+
+
+
