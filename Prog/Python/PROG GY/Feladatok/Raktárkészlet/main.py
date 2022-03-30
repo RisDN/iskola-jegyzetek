3a0d9f01a712456d031546b86626e56d9292886c
Ris <64377188+RisDN@users.noreply.github.com>
2022-03-30 12:25:34 +0200

    Update main.py

--- Prog/Python/PROG GY/Feladatok/Raktárkészlet/main.py
+++ Prog/Python/PROG GY/Feladatok/Raktárkészlet/main.py
@@ -9,15 +9,16 @@ for i in open('raktarkeszlet.txt', 'r', encoding="utf-8"):
 
 
 while menu:
-    print('\nMenü\n1. Raktárkészlet listázása\n2. A raktár termékeinek összértéke \n3. Kereső \n4. Hiánycikk kereső')
+    print('\nMenü\n1. Raktárkészlet listázása\n2. A raktár termékeinek összértéke \n3. Kereső \n4. Hiánycikk kereső\n5. Kilépés')
     kerdes = input('Válassz egyet: ')
     try:
         kerdes = int(kerdes)
-        if kerdes < 5 and kerdes > 0:
+        if kerdes < 6 and kerdes > 0:
             if kerdes == 1: m.raktarKeszlet(termekek)
             if kerdes == 2: print(f'\nTermékek összértékek: {m.osszertek(termekek)} ft')
             if kerdes == 3: m.kereses(termekek)
             if kerdes == 4: m.hianyzo(termekek)
+            if kerdes == 5: menu = False
         else:
             print('1-4-ig adj meg számot!')
     except:
