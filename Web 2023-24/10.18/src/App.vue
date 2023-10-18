8b881005da8a95c9cd79c9d5204ddb2209da755c
Ris <64377188+RisDN@users.noreply.github.com>
2023-10-18 09:36:59 +0200

    cica

---
+++ Web 2023-24/10.18/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+import data from "./assets/data.json";
+import Car from "./components/Car.vue"
+</script>
+
+<template>
+  <main>
+   
+    <table>
+      <tr class="asd">
+        <th>Gyártó</th>
+        <th>Modell</th>
+        <th>Szín</th>
+        <th>Ár</th>
+        <th>Gyártás éve</th>
+      </tr>
+        <Car v-for="car in data.cars" 
+          :brand="car.brand" 
+          :model="car.model"
+          :color="car.color"
+          :price="car.price"
+          :year="car.year"
+          />
+    </table>
+
+
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Web 2023-24/10.18/src/components/Car.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(["brand", "model", "color", "price", "year"])
+
+</script>
+
+<template>
+
+    <tr>
+        <td>{{ brand }}</td> 
+        <td>{{ model }}</td> 
+        <td :style="{backgroundColor: color}"></td>
+        <td>{{ price }}</td>
+        <td>{{ year }}</td>
+    </tr>
+
+</template>
+
