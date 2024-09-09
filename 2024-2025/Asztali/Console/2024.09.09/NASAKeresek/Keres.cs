a3702c1129f28f374ca3d2bfcb09828b513b62af
Ris <64377188+RisDN@users.noreply.github.com>
2024-09-09 13:17:00 +0200

    2024 09 09 NASA C# JZ

---
+++ 2024-2025/Asztali/Console/2024.09.09/NASAKeresek/Keres.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Globalization;
+
+namespace NASAKeresek
+{
+
+    internal class Keres
+    {
+        private static List<Keres> keresek = new List<Keres>();
+
+        public string Cim { get; set; }
+        public DateTime Ido { get; set; }
+        public string Kep { get; set; }
+        public int Kod { get; set; }
+        public int? Meret { get; set; }
+
+        public bool Domain => !char.IsNumber(Cim[^1]);
+
+        public Keres(string line)
+        {
+            string[] splitted = line.Split("*");
+            Cim = splitted[0];
+
+            Ido = DateTime.ParseExact(splitted[1], "dd/MMM/yyyy:HH:mm:ss", CultureInfo.InvariantCulture);
+            Kep = splitted[2];
+
+            string[] splitted2 = splitted[3].Split(" ");
+            Kod = int.Parse(splitted2[0]);
+            Meret = splitted2[1] == "-" ? null : int.Parse(splitted2[1]);
+
+            keresek.Add(this);
+        }
+
+        public static List<Keres> GetKeresek()
+        {
+            return keresek;
+        }
+    }
+}
--- 2024-2025/Asztali/Console/2024.09.09/NASAKeresek/Program.cs
+++ 2024-2025/Asztali/Console/2024.09.09/NASAKeresek/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StreamReader reader = new StreamReader(@"NASAlog.txt");
+            string line;
+            while((line = reader.ReadLine()) != null) 
+            {
+                new Keres(line);
+            }
+
+            List<Keres> keresek = Keres.GetKeresek();
+            
+            Console.WriteLine("5. feladat: Kérések száma: " + keresek.Count);
+
+            int? sizeSum = keresek.Sum(k => k.Meret);
+
+            Console.WriteLine("6. feladat: Válaszok összes mérete: " + sizeSum.ToString() + " byte");
+
+            int hasDomain = keresek.Count(k => k.Domain);
+            Console.WriteLine($"7. feladat: domain-es kérések: {hasDomain / (float) keresek.Count * 100:0.00}%");
+
+            var stats = keresek.GroupBy(k => k.Kod);
+            Console.WriteLine("9. feladat: statisztika");
+            foreach(var stat in stats)
+            {
+                Console.WriteLine($"\t{stat.Key}: {stat.Count()} db");
+            }
+
         }
     }
 }
