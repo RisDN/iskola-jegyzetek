3af578e82190cb1556e8911e87c23fb2d7c73a30
Ris <64377188+RisDN@users.noreply.github.com>
2024-10-10 13:17:29 +0200

    dolgozok C# Forms SE 2024.10.10

---
+++ 2024-2025/Asztali/Forms/2024.10.10/dolgozok/Dolgozo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozok
+{
+    internal class Dolgozo
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Sex { get; set; }
+
+
+        public Dolgozo(string line)
+        {
+            string[] parts = line.Split(';');
+            Name = parts[0];
+            Age = int.Parse(parts[1]);
+            City = parts[2];
+            Sex = parts[3];
+        }
+
+    }
+}
---
+++ 2024-2025/Asztali/Forms/2024.10.10/dolgozok/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dolgozok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            StreamReader streamReader = new StreamReader("dolgozok.txt");
+
+            List<Dolgozo> list = new List<Dolgozo>();
+
+            while (!streamReader.EndOfStream)
+            {
+                list.Add(new Dolgozo(streamReader.ReadLine())); 
+            }
+            streamReader.Close();
+
+            double avg = list.Average(x => x.Age);
+            label3.Text = "Dolgozók átlagéletkora: " + avg.ToString("0.0") + " év";
+            label2.Text = "Dolgozói létszám: " + list.Count.ToString();
+
+
+
+            foreach (Dolgozo dolgozo in list)
+            {
+                richTextBox1.Text += $"{dolgozo.Name}: {dolgozo.Age} év, {dolgozo.City}, {dolgozo.Sex}\n";
+            }
+
+
+        }
+    }
+}
