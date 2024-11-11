8acc37437b54d7c7ed0ba417167ff0cfc50d8b5b
Ris <64377188+RisDN@users.noreply.github.com>
2024-11-11 12:49:35 +0100

    halak

---
+++ 2024-2025/Asztali/Console/2024.11.11/halakandpalinka/Fish.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace halak
+{
+    internal class Fish
+    {
+        private int top;
+        private int depth;
+        private float weight;
+        private bool weightIsSet = false;
+
+        public float Weight { 
+            get => weight;
+            set {
+
+                if(value > 40.0 || value < 0.5)
+                {
+                    throw new Exception($"A hal 'weight' ({value}) értékének 0.5 és 40.0 között kell lennie.");
+                }
+
+                if (weightIsSet && (value < weight * 0.9 || value > weight * 1.1))
+                {
+                    throw new Exception($"A hal 'weight' ({value}) értékének a korábbi érték 10%-a között kell lennie.");
+                }
+                weightIsSet = true;
+                weight = value; 
+            }
+        }
+        public bool Predator { get; }
+        public int Top { 
+            get => top;
+            set
+            {
+                if (value > 400 || value < 0)
+                {
+                    throw new Exception($"A hal 'top' ({value}) értékének 0 és 400 között kell lennie.");
+                }
+                top = value;
+            }
+        }
+        public int Depth { 
+            get => depth;
+            set
+            {
+                if (value > 400 || value < 10)
+                {
+                    throw new Exception($"A hal 'depth' ({value}) értékének 10 és 400 között kell lennie.");
+                }
+
+                depth = value;
+            }
+        }
+        public string Species { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Species} ({(Predator ? "Húsevő" : "Növényevő")}) [{Top} - {Top+Depth} cm] {Weight} kg";
+        }
+
+        public Fish(float weight, bool predator, int top, int depth, string species)
+        {
+            Weight = weight;
+            Predator = predator;
+            Top = top;
+            Depth = depth;
+            Species = species ?? throw new ArgumentNullException(nameof(species));
+        }
+    }
+}
---
+++ 2024-2025/Asztali/Console/2024.11.11/halakandpalinka/Program.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace halak
+{
+    internal class Program
+    {
+        private static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+
+            List<Fish> list = new List<Fish>();
+            List<string> species = new List<string>();
+
+            species.Add("Busa");
+            species.Add("Keszeg");
+            species.Add("Ponty");
+
+            for (int i = 0; i < 100; i++)
+            {
+               
+                bool predator = rnd.Next(0, 100) < 90;
+                float weight = (float) rnd.NextDouble() * 39.5f + 0.5f;
+                int top = rnd.Next(0, 400);
+                int depth = rnd.Next(10, 400);
+                string speciesName = species[rnd.Next(0, species.Count)];   
+                list.Add(new Fish(weight, predator, top, depth, speciesName));
+
+            }
+
+            int predatorCount = list.FindAll(k => k.Predator).Count;
+            int nonPredatorCount = list.Count - predatorCount;
+            int canSwimUnder110 = list.FindAll(k => k.Depth > 110).Count;
+
+            Console.WriteLine($"Húsevő halak száma: {predatorCount}");
+            Console.WriteLine($"Növényevő halak száma: {nonPredatorCount}");
+            Console.WriteLine($"110 cm-nél mélyebben úszó halak száma: {canSwimUnder110}");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Fish fish1 = list[rnd.Next(0, list.Count)];
+                Fish fish2 = list[rnd.Next(0, list.Count)];
+
+                if(fish1 == fish2)
+                {
+                    continue;
+                }
+
+                if (fish1.Predator == fish2.Predator)
+                {
+                    continue;
+                }
+
+                if(!((fish1.Predator && !fish2.Predator) || (!fish1.Predator && fish2.Predator))) {
+                    continue;
+                }
+                Fish preadtor = fish1.Predator ? fish1 : fish2;
+                Fish nonPredator = fish1.Predator ? fish2 : fish1;
+
+               
+                if(nonPredator.Top >= preadtor.Top && nonPredator.Top <= preadtor.Top + preadtor.Depth)
+                {
+                    continue;
+                }
+
+                bool eat = rnd.Next(0, 100) < 30;
+
+                if(eat)
+                {
+                    list.Remove(nonPredator);
+                    //preadtor.Weight += 2;
+                    Console.WriteLine(preadtor + " megette: " + nonPredator);
+                }
+
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
