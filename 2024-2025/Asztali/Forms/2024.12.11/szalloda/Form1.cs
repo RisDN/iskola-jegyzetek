1b249280fd6a64081bff569b5701d89a65c7e5de
Ris <64377188+RisDN@users.noreply.github.com>
2024-12-11 09:38:54 +0100

    vendegek 2024.12.11 SE C# Forms

---
+++ 2024-2025/Asztali/Forms/2024.12.11/szalloda/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace szalloda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Vendeg.Load();
+            DisplayData();
+            for(int i = 1; i < 11; i++)
+            {
+                ejszakak_input.Items.Add(i);
+            }
+            ejszakak_input.SelectedIndex = 0;
+        }
+
+
+        public void DisplayData()
+        {
+            adatok_output.Text = null;
+            foreach (Vendeg vendeg in Vendeg.vendegek)
+            {
+                adatok_output.Text += $"{vendeg.Nev};{vendeg.EjszakakSzama};{vendeg.VendegekSzama};{vendeg.EllatasTipusa}\n";
+            }
+
+            sum_vendegej.Text = "Vendég éjszakák száma összesen: ";
+            sum_vendegej.Text += Vendeg.vendegek.Sum(x => x.VendegEjszakakSzamitas()).ToString();
+
+            teljesellatasok.Text = "A teljes ellátást igénybevevők száma: ";
+            teljesellatasok.Text += Vendeg.vendegek.Count(x => x.EllatasTipusa == "p").ToString();
+
+            torzsvendegek_output.Text = null;
+            List<Vendeg> teljesEllatask = Vendeg.vendegek.FindAll(x => x.EllatasTipusa == "p");
+            foreach(Vendeg vendeg in teljesEllatask) {
+                torzsvendegek_output.Text += $"{vendeg.Nev}\n";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nev = nev_input.Text;
+
+            if(nev.Length == 0)
+            {
+                DisplayHiba("Adja meg a vendég nevét!");
+                return;
+            }
+
+            int ejszakak = 0;
+            try
+            {
+                ejszakak = int.Parse(ejszakak_input.Text);
+            }
+            catch (Exception)
+            {
+                DisplayHiba("Adja meg az éjszakák számát!");
+                return;
+            }
+
+            int vendegekSzama = 0;
+            try
+            {
+                vendegekSzama = int.Parse(vendegek_input.Text);
+            }
+            catch (Exception)
+            {
+                DisplayHiba("Adja meg a vendégek számát!");
+                return;
+            }
+
+        }
+
+        public void DisplayHiba(string message)
+        {
+            MessageBox.Show(message, "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ 2024-2025/Asztali/Forms/2024.12.11/szalloda/Vendeg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szalloda
+{
+    internal class Vendeg
+    {
+        public static List<Vendeg> vendegek = new List<Vendeg>();
+
+        public string Nev { get; set; }
+        public int EjszakakSzama { get; set; }
+        public int VendegekSzama { get; set; }
+        public string EllatasTipusa { get; set; }
+
+        public Vendeg(string nev, int ejszakakSzama, int vendegekSzama, string ellatasTipusa)
+        {
+            Nev = nev;
+            EjszakakSzama = ejszakakSzama;
+            VendegekSzama = vendegekSzama;
+            EllatasTipusa = ellatasTipusa;
+
+            vendegek.Add(this);
+        }
+
+        public int VendegEjszakakSzamitas()
+        {
+            return EjszakakSzama * VendegekSzama;
+        }
+
+        public static void Load()
+        {
+            StreamReader streamReader = new StreamReader("vendegek.txt");
+            while (!streamReader.EndOfStream)
+            {
+                string[] sor = streamReader.ReadLine().Split(';');
+                new Vendeg(sor[0], int.Parse(sor[1]), int.Parse(sor[2]), sor[3]);
+            }
+            streamReader.Close();
+        }
+
+        public static void Save()
+        {
+            StreamWriter streamWriter = new StreamWriter("vendegek.txt");
+            foreach (Vendeg vendeg in vendegek)
+            {
+                streamWriter.WriteLine($"{vendeg.Nev};{vendeg.EjszakakSzama};{vendeg.VendegekSzama};{vendeg.EllatasTipusa}");
+            }
+            streamWriter.Close();
+        }
+
+    }
+}
