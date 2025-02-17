7278155c969a20490f74a1be1c50a85615d5f564
Ris <64377188+RisDN@users.noreply.github.com>
2025-02-17 12:25:07 +0100

    course C# JZ 2025.02.17

---
+++ 2024-2025/Asztali/Console/2025.02.17/course/Student.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace course
+{
+
+    internal enum Subject
+    {
+        Hálózat = 0,
+        Mobil = 1,
+        Frontend = 2,
+        Backend = 3
+    }
+
+    internal class Student
+    {
+        public string Name { get; set; }
+        public char Sex { get; set; }
+        public int Paid { get; set; }
+        public List<int> Results { get; set; }
+
+
+        public Student(string name, char sex, int paid, List<int> results)
+        {
+            Name = name;
+            Sex = sex;
+            Paid = paid;
+            Results = results;
+        }
+
+        public double GetAvarageResult()
+        {
+            return Results.Average();
+        }
+
+        public string GetLastName()
+        {
+            return Name.Split(' ')[1];
+        }
+
+        public int GetSubjectResult(Subject subject)
+        {
+            return Results[(int)subject];
+        }
+
+        public int GetResultsCombined()
+        {
+            return Results.Sum();
+        }
+
+        public int GetWebResults()
+        {
+            return GetSubjectResult(Subject.Frontend) + GetSubjectResult(Subject.Backend);
+        }
+
+        public bool IsAllPaid()
+        {
+            return Paid >= 2600;       
+        }
+
+        public bool Has100Percent()
+        {
+            return Results.Contains(100);
+        }
+
+        public bool DidFailAnySubject()
+        {
+            return GetFailedSubjects().Count != 0;
+        }
+
+        public List<Subject> GetFailedSubjects()
+        {
+            List<Subject> failedSubjects = new List<Subject>();
+
+            foreach(Subject subject in Enum.GetValues(typeof(Subject)))
+            {
+                if (GetSubjectResult(subject) < 51)
+                {
+                    failedSubjects.Add(subject);
+                }
+            }
+
+            return failedSubjects;
+
+
+        }
+
+    }
+}
