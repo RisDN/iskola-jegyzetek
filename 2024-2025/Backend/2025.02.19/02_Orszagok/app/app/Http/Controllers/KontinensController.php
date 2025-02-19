79a5176b27e51de6ba531782533a9f6edea5465b
Ris <64377188+RisDN@users.noreply.github.com>
2025-02-19 13:07:35 +0100

    kontinens page

--- 2024-2025/Backend/2025.02.19/02_Orszagok/app/app/Http/Controllers/KontinensController.php
+++ 2024-2025/Backend/2025.02.19/02_Orszagok/app/app/Http/Controllers/KontinensController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\KontinensModel;
+use App\Models\OrszagModel;
 
 class KontinensController extends Controller
 {
     public function GetKontinensById($id) {
+
         return view('kontinens', [
             "result" => KontinensModel::all()->where("kontinens_id", "=", $id)->first(),
+            "darabszam" => count(OrszagModel::all()->where("kontinens_id", "=", $id)),
+            "kontinensek" => KontinensModel::all(),
         ]);
+
     }
 }
--- 2024-2025/Backend/2025.02.19/02_Orszagok/app/resources/views/kontinens.blade.php
+++ 2024-2025/Backend/2025.02.19/02_Orszagok/app/resources/views/kontinens.blade.php
@@ -1,6 +1,12 @@
 @extends('layout')
 
 @section('content')
+    <div class="text-center m-5">
+        @foreach ($kontinensek as $kontinens)
+            <a class="btn btn-outline-dark" href="{{ '/kontinensek/' . $kontinens->kontinens_id }}">{{ $kontinens->nev }}</a>
+        @endforeach
+    </div>
+
     <h2 class="text-center display-6 py-3">{{ $result->nev }}</h2>
 
     <div class="container">
@@ -10,7 +16,37 @@
                 <img src="{{ asset('/img/' . $result->kontinens_id . '.jpg') }}" class="img-fluid" alt="gec">
 
             </div>
-            <div class="col">asddas</div>
+            <div class="col">
+
+                <table class="table table-striped table-bordered">
+                    <tr>
+                        <th>Területe</th>
+                        <td>{{ number_format($result->terulet, 0, ' ', ' ') }} km2</td>
+                    </tr>
+
+                    <tr>
+                        <th>Legmagasabb pontja</th>
+                        <td>{{ number_format($result->max, 0, ' ', ' ') }} m - {{ $result->max_nev }}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Legmélyebb pontja</th>
+                        <td>{{ number_format($result->min, 0, ' ', ' ') }} m - {{ $result->min_nev }}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Leghosszabb folyó</th>
+                        <td>{{ number_format($result->folyo, 0, ' ', ' ') }} m - {{ $result->folyo_nev }}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Országok száma</th>
+                        <td>{{ $darabszam }} db</td>
+                    </tr>
+
+                </table>
+
+            </div>
         </div>
     </div>
 @endsection
