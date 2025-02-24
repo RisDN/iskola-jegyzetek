baaf2145bfc5b732b985f04e549d0afa9b3475d8
Ris <64377188+RisDN@users.noreply.github.com>
2025-02-24 13:30:30 +0100

    kms

---
+++ 2024-2025/Asztali/Console/2025.02.24/keresztrejtveny/KeresztrejtvenyRacs.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keresztrejtveny
+{
+    internal class KeresztrejtvenyRacs
+    {
+
+
+        private List<string> Adatsorok = new List<string>();
+        private char[,] Racs;
+        private int[,] Sorszamok;
+
+        public int OszlopokDb { get; set; }
+        public int SorokDb { get; set; } 
+
+
+        public KeresztrejtvenyRacs(string forras)
+        {
+            BeolvasAdatsorok(forras);
+        }
+
+        public int LeghosszabbFuggolegesSzo()
+        {
+            int max = -1;
+            for(int oszlop = 0; oszlop < OszlopokDb -1; oszlop++) { 
+  
+                for(int sor = 0; sor < SorokDb -1; sor++)
+                {
+                    Console.WriteLine($"[{oszlop}:{sor}]"); // ezen a ponton menjünk el lefele ameddig az oszlop tart
+                    int lementdarab = 0;
+                    for(int x = oszlop; x < OszlopokDb -1; x++)
+                    {
+              
+                        Console.WriteLine($"Lefelé: [{oszlop}:{x}]");
+                        if (Racs[oszlop, x].Equals('#'))
+                        {
+                            Console.WriteLine("Lement " + lementdarab + "x alkalommal");
+                            continue;
+                        }
+                        lementdarab++;
+
+                        if (lementdarab > max)
+                        {
+                            max = lementdarab;
+                        }
+
+                    }
+                }
+
+            }
+
+            return max;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < OszlopokDb; i++)
+            {
+                for (int y = 0; y < SorokDb; y++)
+                {
+                    string character = "";
+
+                    if (Racs[i, y].Equals('-')) {
+                        character = "[]";
+                    } else if (Racs[i, y].Equals('#'))
+                    {
+                        character = "##";
+                    }
+
+                    Console.Write(character);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public void BeolvasAdatsorok(string forras)
+        {
+
+            StreamReader reader = new StreamReader(@forras);
+            int lines = 0;
+            while(!reader.EndOfStream)
+            {
+                lines++;
+                Adatsorok.Add(reader.ReadLine());
+            }
+            int lineLength = Adatsorok[0].Length;
+
+            OszlopokDb = lineLength;
+            SorokDb = lines;
+
+            Racs = new char[OszlopokDb, SorokDb];
+            Sorszamok = new int[OszlopokDb, SorokDb];
+
+            Console.WriteLine($"{OszlopokDb}x{SorokDb}");
+
+            FeltoltRacs();
+        }
+
+        public void FeltoltRacs()
+        {
+            Console.WriteLine("oszlopok: " + OszlopokDb);
+            Console.WriteLine("sorok: " + SorokDb);
+            for(int i = 0; i < OszlopokDb; i++)
+            {
+
+                string line = Adatsorok[i];
+                for (int y = 0; y < SorokDb; y++)
+                {
+  
+                    char[] chars = line.ToCharArray();
+
+                    foreach(char character in chars)
+                    {
+                        Racs[i, y] = character;
+                    }
+
+                }
+            }
+
+        }
+
+    }
+}
---
+++ 2024-2025/Asztali/Console/2025.02.24/keresztrejtveny/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keresztrejtveny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            KeresztrejtvenyRacs rejtveny =  new KeresztrejtvenyRacs("kr1.txt");
+
+            Console.WriteLine("rejtveny 1: ");
+            rejtveny.Show();
+
+
+            KeresztrejtvenyRacs rejtveny2 = new KeresztrejtvenyRacs("kr2.txt");
+
+            Console.WriteLine("\nrejtveny 2:");
+            rejtveny2.Show();
+
+
+            //Console.WriteLine("5. feladat: A keresztrejtvény mérete:");
+            //Console.WriteLine("\tSorok száma: " + rejtveny.SorokDb);
+            //Console.WriteLine("\tOszlopok száma: " + rejtveny.OszlopokDb);
+
+            //Console.WriteLine("6. feladat: A beolvasott keresztrejtvény");
+            //rejtveny.Show();
+
+            //Console.WriteLine($"7. feladat: A leghosszabb függ.: {rejtveny.LeghosszabbFuggolegesSzo()} karakter");
+
+            Console.ReadKey();
+        }
+    }
+}
